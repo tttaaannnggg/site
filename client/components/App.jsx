0998ce44862f118d7b35ec2f23ba3c7246aeae89
tang <eytang8@gmail.com>
2019-08-18 08:12:43 -0700

    continuing to develop filestructure

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+/*
+ * @module      App.jsx
+ * @author      tang
+ * @date        08/18/2019
+ * @description hook into DOM for portfolio
+ *
+ */
+import React from 'react';
+import Portfolio from './pages/Portfolio.jsx';
 
 const App = (props)=>{
   return(
-        <h1> hello world </h1>
+      <Portfolio/>
       )
 }
 
---
+++ client/components/pages/Portfolio.jsx
@@ -0,0 +1,22 @@
+/*
+ * @module      Portfolio
+ * @author      tang
+ * @date        08/18/2019
+ * @description renders main page view, incl header, intro, portfolio
+ *
+ */
+
+const Portfolio = (props)=>{
+  return(
+        <div>
+          <Header>
+          </Header>
+          <Intro>
+          </Intro>
+          <Portfolio>
+          </Portfolio>
+        </div>
+      )
+}
+
+export default Portfolio
